dbe05e7c3f637096bd0217c5839067bddbfe87f2
ankit-gadhwal <ankitgadhwal2004.com>
2026-04-30 12:56:21 +0530

    unittest

--- tests/model_test.py
+++ tests/model_test.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 # Load Dagshub token from environment variables for secure access
 # The Dagshub token is required for authentication when interacting with the DAgshub Mlflow server
-dagshub_token = os.getenv("CI_MlOPS")
+dagshub_token = os.getenv("CI_MLOPS")
 if not dagshub_token:
     raise EnvironmentError("DAGSHUB_TOKEN environment variable is not set")
 
